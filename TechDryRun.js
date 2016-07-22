9ed4ee8f755a3bdcc093cf5e7ceff30fdf724157
Jim Yang <jimyyang@gmail.com>
2016-07-21 21:26:31 -0400

    Add

---
+++ TechDryRun.js
@@ -0,0 +1,38 @@
+//Tech Dry Run
+
+var hrr18 = {
+  name: 'Keeley Nakamoto',
+  anothername: 'Jim',
+  jeffName: 'Jeff Bernstein',
+  yetAnother: 'Dan jrrn eerr n nnrr rner rnnr  eees n oynllolonyro ynronllooo loo
+
+ ro
+nnr
+
+ name: 'Caleb',
+
+  neekon: neekon,
+  per
+
+so
+n
+
+  na
+myyyy
+yy
+PayPe
+
+
+
+y: 'PPayt:yy}:::
+:
+yylo
+ama
+eeeaeema:meeaamaeaaeeaeaaeeeeeeee:e::eee emmea::a esnY
+
+
+
+
+
+
+name: 'Mchael Olorunnisola'
